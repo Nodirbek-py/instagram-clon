524bfc0aee9f1a9a342897190d02d72f24875e2b
nodirbek-py <xesos2001@gmail.com>
2021-05-10 16:06:34 +0500

    auth. is ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Suspense } from "react";
+import Post from "./components/post/post";
+import { db } from "./firebase/firebase";
+import Navbar from "./components/navbar/navbar";
+class App extends React.Component {
+    state = {
+        posts: [],
+        fetching: false,
+    };
+    componentDidMount() {
+        this.setState({
+            fetching: true,
+        });
+        db.collection("posts").onSnapshot((snapshot) => {
+            this.setState({
+                posts: snapshot.docs.map((doc) => {
+                    return doc.data();
+                }),
+                fetching: false,
+            });
+        });
+    }
+    render() {
+        return (
+            <React.Fragment>
+                <Navbar />
+                <div style={{ marginBottom: "100px" }}></div>
+                {this.state.fetching === true ? (
+                    <div>Loading posts</div>
+                ) : (
+                    this.state.posts.map((post, id) => {
+                        return (
+                            <Post
+                                key={id}
+                                username={post.username}
+                                caption={post.caption}
+                                avatar={post.avatar}
+                                image={post.image}
+                            />
+                        );
+                    })
+                )}
+                {console.log(this.state.posts[0])}
+            </React.Fragment>
+        );
+    }
 }
-
 export default App;
---
+++ src/firebase/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+const firebaseConfig = {
+    apiKey: "AIzaSyAnbMruC-uF2WaYEoEE5jT_dVcYpy_GwN0",
+    authDomain: "instagram-clone-3eef2.firebaseapp.com",
+    projectId: "instagram-clone-3eef2",
+    storageBucket: "instagram-clone-3eef2.appspot.com",
+    messagingSenderId: "940400193429",
+    appId: "1:940400193429:web:650055e1fd1b519926909c"
+  };
+
+const firebaseapp = firebase.initializeApp(firebaseConfig)
+const db = firebase.firestore()
+const auth = firebase.auth()
+const storage = firebase.storage()
+export {db, auth, storage}
